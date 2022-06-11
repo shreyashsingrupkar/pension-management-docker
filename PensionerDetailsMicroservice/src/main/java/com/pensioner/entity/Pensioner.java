f98266cb12f0fcdddef0e59ea367aac054a05007
shreyash singrupkar <shreyashsingrupkar@gmail.com>
2022-06-11 06:33:11 +0000

    pension management with docker

---
+++ PensionerDetailsMicroservice/src/main/java/com/pensioner/entity/Pensioner.java
@@ -0,0 +1,88 @@
+package com.pensioner.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+
+@Table
+@Entity
+public class Pensioner {
+@Id
+public long aadharno;
+public String name; 
+public String dob;
+public String pan;
+public double salaryearned;//4. SalaryEarned//<Last earned salary by the pensioner>
+public double allowances;//5. Allowances//<Sum of all the allowances>
+public String pensiontype;
+public String bankname;
+public long accountno;
+public String banktype;
+public long getAadharno() {
+	return aadharno;
+}
+public void setAadharno(long aadharno) {
+	this.aadharno = aadharno;
+}
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public String getDob() {
+	return dob;
+}
+public void setDob(String dob) {
+	this.dob = dob;
+}
+public String getPan() {
+	return pan;
+}
+public void setPan(String pan) {
+	this.pan = pan;
+}
+public double getSalaryearned() {
+	return salaryearned;
+}
+public void setSalaryearned(double salaryearned) {
+	this.salaryearned = salaryearned;
+}
+public double getAllowances() {
+	return allowances;
+}
+public void setAllowances(double allowances) {
+	this.allowances = allowances;
+}
+public String getPensiontype() {
+	return pensiontype;
+}
+public void setPensiontype(String pensiontype) {
+	this.pensiontype = pensiontype;
+}
+public String getBankname() {
+	return bankname;
+}
+public void setBankname(String bankname) {
+	this.bankname = bankname;
+}
+public long getAccountno() {
+	return accountno;
+}
+public void setAccountno(long accountno) {
+	this.accountno = accountno;
+}
+public String getBanktype() {
+	return banktype;
+}
+public void setBanktype(String banktype) {
+	this.banktype = banktype;
+}
+
+
+
+}
